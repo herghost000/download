9be978980d79495319ed83852f6bd39802638f92
luotian <77209302@qq.com>
2018-05-15 18:32:16 +0800

    c1 c2 undefined bug fix

--- download.js
+++ download.js
@@ -116,9 +116,8 @@
             return utftext;
         },
         _utf8_decode: function(utftext) {
-            var string = "";
-            var i = 0;
-            var c = c1 = c2 = 0;
+            var string = "",i = 0,c,c1,c2;
+                c = c1 = c2 = 0;
 
             while(i < utftext.length) {
 
