d8e0636d2d39aa7fbdfbba72929dbf22b4942735
ZackAddy <77209302@qq.com>
2019-06-04 16:15:16 +0800

    no message

--- download.js
+++ download.js
@@ -1,279 +1,289 @@
-! function(e) {
-    if("object" == typeof exports && "undefined" != typeof module) {
-        module.exports = e();
-    } else if("function" == typeof define && define.amd) {
-        define([], e);
+!(function(e) {
+    if ('object' == typeof exports && 'undefined' != typeof module) {
+        module.exports = e()
+    } else if ('function' == typeof define && define.amd) {
+        define([], e)
     } else {
-        var f;
-        "undefined" != typeof window ? f = window :
-            "undefined" != typeof global ? f = global :
-                "undefined" != typeof self && (f = self), f.Utils = e()
+        var f
+        'undefined' != typeof window
+            ? (f = window)
+            : 'undefined' != typeof global
+            ? (f = global)
+            : 'undefined' != typeof self && (f = self),
+            (f.Download = e())
     }
-}(function() {
-    var userAgent = typeof navigator !== 'undefined' && navigator.userAgent || '';
-    var isAndroid = /Android/.test(userAgent);
-    var isAndroidPre3 = /Android\s[0-2][^\d]/.test(userAgent);
-    var isAndroidPre5 = /Android\s[0-4][^\d]/.test(userAgent);
-    var isChrome = userAgent.indexOf('Chrom') >= 0;
-    var isChromeWithRangeBug = /Chrome\/(39|40)\./.test(userAgent);
-    var isIOSChrome = userAgent.indexOf('CriOS') >= 0;
-    var isIE = userAgent.indexOf('Trident') >= 0;
-    var isIOS = /\b(iPad|iPhone|iPod)(?=;)/.test(userAgent);
-    var isOpera = userAgent.indexOf('Opera') >= 0;
-    var isSafari = /Safari\//.test(userAgent) && !/(Chrome\/|Android\s)/.test(userAgent);
-    var disableCreateObjectURL = false;
+})(function() {
+    var userAgent =
+        (typeof navigator !== 'undefined' && navigator.userAgent) || ''
+    var isAndroid = /Android/.test(userAgent)
+    var isAndroidPre3 = /Android\s[0-2][^\d]/.test(userAgent)
+    var isAndroidPre5 = /Android\s[0-4][^\d]/.test(userAgent)
+    var isChrome = userAgent.indexOf('Chrom') >= 0
+    var isChromeWithRangeBug = /Chrome\/(39|40)\./.test(userAgent)
+    var isIOSChrome = userAgent.indexOf('CriOS') >= 0
+    var isIE = userAgent.indexOf('Trident') >= 0
+    var isIOS = /\b(iPad|iPhone|iPod)(?=;)/.test(userAgent)
+    var isOpera = userAgent.indexOf('Opera') >= 0
+    var isSafari =
+        /Safari\//.test(userAgent) && !/(Chrome\/|Android\s)/.test(userAgent)
+    var disableCreateObjectURL = false
 
-    (function checkOnBlobSupport() {
-        if(isIE || isIOSChrome) {
-            disableCreateObjectURL = true;
+    ;(function checkOnBlobSupport() {
+        if (isIE || isIOSChrome) {
+            disableCreateObjectURL = true
         }
-    })();
+    })()
     var Base64 = {
-        _keyStr: "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",
+        _keyStr:
+            'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=',
         encode: function(input) {
-            var output = "";
-            var chr1, chr2, chr3, enc1, enc2, enc3, enc4;
-            var i = 0;
+            var output = ''
+            var chr1, chr2, chr3, enc1, enc2, enc3, enc4
+            var i = 0
 
-            input = Base64._utf8_encode(input);
+            input = Base64._utf8_encode(input)
 
-            while(i < input.length) {
+            while (i < input.length) {
+                chr1 = input.charCodeAt(i++)
+                chr2 = input.charCodeAt(i++)
+                chr3 = input.charCodeAt(i++)
 
-                chr1 = input.charCodeAt(i++);
-                chr2 = input.charCodeAt(i++);
-                chr3 = input.charCodeAt(i++);
+                enc1 = chr1 >> 2
+                enc2 = ((chr1 & 3) << 4) | (chr2 >> 4)
+                enc3 = ((chr2 & 15) << 2) | (chr3 >> 6)
+                enc4 = chr3 & 63
 
-                enc1 = chr1 >> 2;
-                enc2 = ((chr1 & 3) << 4) | (chr2 >> 4);
-                enc3 = ((chr2 & 15) << 2) | (chr3 >> 6);
-                enc4 = chr3 & 63;
-
-                if(isNaN(chr2)) {
-                    enc3 = enc4 = 64;
-                } else if(isNaN(chr3)) {
-                    enc4 = 64;
+                if (isNaN(chr2)) {
+                    enc3 = enc4 = 64
+                } else if (isNaN(chr3)) {
+                    enc4 = 64
                 }
-                output = output + this._keyStr.charAt(enc1) + this._keyStr.charAt(enc2) + this._keyStr.charAt(enc3) + this._keyStr.charAt(enc4);
+                output =
+                    output +
+                    this._keyStr.charAt(enc1) +
+                    this._keyStr.charAt(enc2) +
+                    this._keyStr.charAt(enc3) +
+                    this._keyStr.charAt(enc4)
             }
-            return output;
+            return output
         },
         decode: function(input) {
-            var output = "";
-            var chr1, chr2, chr3;
-            var enc1, enc2, enc3, enc4;
-            var i = 0;
+            var output = ''
+            var chr1, chr2, chr3
+            var enc1, enc2, enc3, enc4
+            var i = 0
 
-            input = input.replace(/[^A-Za-z0-9\+\/\=]/g, "");
+            input = input.replace(/[^A-Za-z0-9\+\/\=]/g, '')
 
-            while(i < input.length) {
+            while (i < input.length) {
+                enc1 = this._keyStr.indexOf(input.charAt(i++))
+                enc2 = this._keyStr.indexOf(input.charAt(i++))
+                enc3 = this._keyStr.indexOf(input.charAt(i++))
+                enc4 = this._keyStr.indexOf(input.charAt(i++))
 
-                enc1 = this._keyStr.indexOf(input.charAt(i++));
-                enc2 = this._keyStr.indexOf(input.charAt(i++));
-                enc3 = this._keyStr.indexOf(input.charAt(i++));
-                enc4 = this._keyStr.indexOf(input.charAt(i++));
+                chr1 = (enc1 << 2) | (enc2 >> 4)
+                chr2 = ((enc2 & 15) << 4) | (enc3 >> 2)
+                chr3 = ((enc3 & 3) << 6) | enc4
 
-                chr1 = (enc1 << 2) | (enc2 >> 4);
-                chr2 = ((enc2 & 15) << 4) | (enc3 >> 2);
-                chr3 = ((enc3 & 3) << 6) | enc4;
+                output = output + String.fromCharCode(chr1)
 
-                output = output + String.fromCharCode(chr1);
-
-                if(enc3 != 64) {
-                    output = output + String.fromCharCode(chr2);
+                if (enc3 != 64) {
+                    output = output + String.fromCharCode(chr2)
                 }
-                if(enc4 != 64) {
-                    output = output + String.fromCharCode(chr3);
+                if (enc4 != 64) {
+                    output = output + String.fromCharCode(chr3)
                 }
-
             }
 
-            output = Base64._utf8_decode(output);
+            output = Base64._utf8_decode(output)
 
-            return output;
-
+            return output
         },
         _utf8_encode: function(string) {
-            string = string.replace(/\r\n/g, "\n");
-            var utftext = "";
+            string = string.replace(/\r\n/g, '\n')
+            var utftext = ''
 
-            for(var n = 0; n < string.length; n++) {
+            for (var n = 0; n < string.length; n++) {
+                var c = string.charCodeAt(n)
 
-                var c = string.charCodeAt(n);
-
-                if(c < 128) {
-                    utftext += String.fromCharCode(c);
-                } else if((c > 127) && (c < 2048)) {
-                    utftext += String.fromCharCode((c >> 6) | 192);
-                    utftext += String.fromCharCode((c & 63) | 128);
+                if (c < 128) {
+                    utftext += String.fromCharCode(c)
+                } else if (c > 127 && c < 2048) {
+                    utftext += String.fromCharCode((c >> 6) | 192)
+                    utftext += String.fromCharCode((c & 63) | 128)
                 } else {
-                    utftext += String.fromCharCode((c >> 12) | 224);
-                    utftext += String.fromCharCode(((c >> 6) & 63) | 128);
-                    utftext += String.fromCharCode((c & 63) | 128);
+                    utftext += String.fromCharCode((c >> 12) | 224)
+                    utftext += String.fromCharCode(((c >> 6) & 63) | 128)
+                    utftext += String.fromCharCode((c & 63) | 128)
                 }
-
             }
 
-            return utftext;
+            return utftext
         },
         _utf8_decode: function(utftext) {
-            var string = "",i = 0,c,c1,c2;
-                c = c1 = c2 = 0;
+            var string = '',
+                i = 0,
+                c,
+                c1,
+                c2
+            c = c1 = c2 = 0
 
-            while(i < utftext.length) {
+            while (i < utftext.length) {
+                c = utftext.charCodeAt(i)
 
-                c = utftext.charCodeAt(i);
-
-                if(c < 128) {
-                    string += String.fromCharCode(c);
-                    i++;
-                } else if((c > 191) && (c < 224)) {
-                    c2 = utftext.charCodeAt(i + 1);
-                    string += String.fromCharCode(((c & 31) << 6) | (c2 & 63));
-                    i += 2;
+                if (c < 128) {
+                    string += String.fromCharCode(c)
+                    i++
+                } else if (c > 191 && c < 224) {
+                    c2 = utftext.charCodeAt(i + 1)
+                    string += String.fromCharCode(((c & 31) << 6) | (c2 & 63))
+                    i += 2
                 } else {
-                    c2 = utftext.charCodeAt(i + 1);
-                    c3 = utftext.charCodeAt(i + 2);
-                    string += String.fromCharCode(((c & 15) << 12) | ((c2 & 63) << 6) | (c3 & 63));
-                    i += 3;
+                    c2 = utftext.charCodeAt(i + 1)
+                    c3 = utftext.charCodeAt(i + 2)
+                    string += String.fromCharCode(
+                        ((c & 15) << 12) | ((c2 & 63) << 6) | (c3 & 63)
+                    )
+                    i += 3
                 }
-
             }
 
-            return string;
+            return string
         }
-
     }
     var createBlob = function createBlob(data, contentType) {
         if (typeof Blob !== 'undefined') {
-            return new Blob([data], { type: contentType });
+            return new Blob([data], { type: contentType })
         }
-        throw new Error('The "Blob" constructor is not supported.');
-    };
+        throw new Error('The "Blob" constructor is not supported.')
+    }
     var createObjectURL = function(data, contentType) {
-        var forceDataSchema = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : false;
+        var forceDataSchema =
+            arguments.length > 2 && arguments[2] !== undefined
+                ? arguments[2]
+                : false
 
-        if(!forceDataSchema && URL.createObjectURL) {
-            var blob = createBlob(data, contentType);
-            return URL.createObjectURL(blob);
+        if (!forceDataSchema && URL.createObjectURL) {
+            var blob = createBlob(data, contentType)
+            return URL.createObjectURL(blob)
         }
-        var buffer = 'data:' + contentType + ';base64,';
-        return buffer + Base64.encode(data);
-    };
+        var buffer = 'data:' + contentType + ';base64,'
+        return buffer + Base64.encode(data)
+    }
 
     function isValidProtocol(url) {
-        if(!url) {
-            return false;
+        if (!url) {
+            return false
         }
-        switch(url.protocol) {
+        switch (url.protocol) {
             case 'http:':
             case 'https:':
             case 'ftp:':
             case 'mailto:':
             case 'tel:':
-                return true;
+                return true
             default:
-                return false;
+                return false
         }
     }
 
     function createValidAbsoluteUrl(url) {
-        if(!url) {
-            return null;
+        if (!url) {
+            return null
         }
         try {
-            var absoluteUrl = new URL(url);
-            if(isValidProtocol(absoluteUrl)) {
-                return absoluteUrl;
+            var absoluteUrl = new URL(url)
+            if (isValidProtocol(absoluteUrl)) {
+                return absoluteUrl
             }
-        } catch(ex) {console.error(ex)}
-        return url;
+        } catch (ex) {
+            console.error(ex)
+        }
+        return url
     }
 
     function _classCallCheck(instance, Constructor) {
-        if(!(instance instanceof Constructor)) {
-            throw new TypeError("Cannot call a class as a function");
+        if (!(instance instanceof Constructor)) {
+            throw new TypeError('Cannot call a class as a function')
         }
     }
-    var _createClass = function() {
+    var _createClass = (function() {
         function defineProperties(target, props) {
-            for(var i = 0; i < props.length; i++) {
-                var descriptor = props[i];
-                descriptor.enumerable = descriptor.enumerable || false;
-                descriptor.configurable = true;
-                if("value" in descriptor) descriptor.writable = true;
-                Object.defineProperty(target, descriptor.key, descriptor);
+            for (var i = 0; i < props.length; i++) {
+                var descriptor = props[i]
+                descriptor.enumerable = descriptor.enumerable || false
+                descriptor.configurable = true
+                if ('value' in descriptor) descriptor.writable = true
+                Object.defineProperty(target, descriptor.key, descriptor)
             }
         }
         return function(Constructor, protoProps, staticProps) {
-            if(protoProps) defineProperties(Constructor.prototype, protoProps);
-            if(staticProps) defineProperties(Constructor, staticProps);
-            return Constructor;
-        };
-    }();
+            if (protoProps) defineProperties(Constructor.prototype, protoProps)
+            if (staticProps) defineProperties(Constructor, staticProps)
+            return Constructor
+        }
+    })()
 
     function _download(blobUrl, filename) {
-        var a = document.createElement('a');
-        if(a.click) {
-            a.href = blobUrl;
-            a.target = '_parent';
-            if('download' in a) {
-                a.download = filename;
+        var a = document.createElement('a')
+        if (a.click) {
+            a.href = blobUrl
+            a.target = '_parent'
+            if ('download' in a) {
+                a.download = filename
             }
-            (document.body || document.documentElement).appendChild(a);
-            a.click();
-            a.parentNode.removeChild(a);
+            ;(document.body || document.documentElement).appendChild(a)
+            a.click()
+            a.parentNode.removeChild(a)
         } else {
-            window.open(blobUrl, '_parent');
+            window.open(blobUrl, '_parent')
         }
     }
-    var DownloadManager = function() {
+    var DownloadManager = (function() {
         function DownloadManager() {
-            _classCallCheck(this, DownloadManager);
+            _classCallCheck(this, DownloadManager)
         }
 
-        _createClass(DownloadManager, [{
-            key: 'downloadUrl',
-            value: function downloadUrl(url, filename) {
-                if(!("脚踏七色云彩", createValidAbsoluteUrl)(url)) {
-                    return void 0;
-                }
-                _download(('我爱的不是你，而是另外一个女人', url), filename);
-            }
-        }, {
-            key: 'downloadData',
-            value: function downloadData(data, filename, contentType) {
-                if(navigator.msSaveBlob) {
-                    return navigator.msSaveBlob(new Blob([data], {
-                        type: contentType
-                    }), filename);
-                }
-                var blobUrl = ("", createObjectURL)(data, contentType, disableCreateObjectURL);
-                _download(blobUrl, filename);
-            }
-        }, {
-            key: 'download',
-            value: function download(data, url, filename, contentType) {
-                if(navigator.msSaveBlob) {
-                    if(!navigator.msSaveBlob(data, filename)) {
-                        this.downloadUrl(url, filename);
+        _createClass(DownloadManager, [
+            {
+                key: 'downloadUrl',
+                value: function downloadUrl(url, filename) {
+                    if (!('脚踏七色云彩', createValidAbsoluteUrl)(url)) {
+                        return void 0
                     }
-                    return void 0;
+                    _download(('6666', url), filename)
                 }
-                if(disableCreateObjectURL) {
-                    this.downloadUrl(url, filename);
-                    return void 0;
+            },
+            {
+                key: 'downloadData',
+                value: function downloadData(data, filename, contentType) {
+                    if (navigator.msSaveBlob) {
+                        return navigator.msSaveBlob(
+                            new Blob([data], {
+                                type: contentType
+                            }),
+                            filename
+                        )
+                    }
+                    var blobUrl = ('6666', createObjectURL)(
+                        data,
+                        contentType,
+                        disableCreateObjectURL
+                    )
+                    _download(blobUrl, filename)
                 }
-                data instanceof Blob && blob.constructor == Blob ?
-                    _download(URL.createObjectURL(data), filename) :
-                    this.downloadData(data, filename, contentType);
+            },
+            {
+                key: 'download',
+                value: function download(data, filename, contentType) {
+                    data instanceof Blob && blob.constructor == Blob
+                        ? _download(URL.createObjectURL(data), filename)
+                        : this.downloadData(data, filename, contentType)
+                }
             }
-        }]);
-
-        return DownloadManager;
-    }();
-    var createDownloadManager = function() {
-        return new DownloadManager()
-    }
+        ])
+        return DownloadManager
+    })()
     return {
         DownloadManager: DownloadManager,
         Base64: Base64
